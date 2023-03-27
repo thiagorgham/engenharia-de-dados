1b15160c2a49ab8cfbd68a4abeba70e314d046f3
Thiago Gham <thiago.gham@andritz.com>
2023-03-26 23:00:05 -0300

    changed build .sh

--- pratica1/scripts/build_lambda_package.sh
+++ pratica1/scripts/build_lambda_package.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash 
 
-cd infrastructure
+cd infrastructure 
 
 # Declara variavel para reutilização nas validações do diretório
 PACKAGE="package"
@@ -37,8 +37,8 @@ cd $PACKAGE
 LAMBDA_FUNCTION=../../etl/lambda_function.py
 
 # Verifica se o arquivo lambda_function.py existe
-if [ -f $LAMBDA_FUNCTION ]
-then
+#if [ -f $LAMBDA_FUNCTION ]
+#then
 	echo "============================================="
 	echo "Copiando função Handler..."
 	cp $LAMBDA_FUNCTION .
@@ -46,6 +46,6 @@ then
 	zip -r9 ../lambda_function_payload.zip . #Compacta o pacote para o deploy
 	echo "Arquivo compactado com sucesso!"
 	echo "============================================="
-fi
+#fi
 
 cd ..
